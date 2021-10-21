33da306106d80153b8f7cf8cead90b4aa097d83a
shrutibellad <shrutixworkz.btm@gmail.com>
2021-10-21 15:54:30 +0530

    files

---
+++ AcessStarter.java
@@ -0,0 +1,12 @@
+package com.xworkz.acess;
+import static com.xworkz.acess.holiday.Festival.*;
+import com.xworkz.acess.holiday.*;
+
+public class AcessStarter {
+public static void main (String[] args) {
+	System.out.println("start main method");
+	System.out.println("holiday");
+	enjoy();
+	Flower.festival.celebration("Shruti");
+}
+}
